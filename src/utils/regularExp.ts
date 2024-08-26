49fe0eeb73ed1f6e2e76716c49fc185c548f7cd6
OlegGrn <grudinol@mail.ru>
2024-08-26 16:51:36 +0200

    26 08 2024

--- src/utils/regularExp.ts
+++ src/utils/regularExp.ts
@@ -1,5 +1,7 @@
 export const regPassword = /^.{2,}/
-export const regEmail = /^\w{2,}/
+
+//export const regEmail = /^\w{2,}/
+export const regEmail = /^[-.\w]+@([\w-]+\.)+[\w-]+$/
 export const regTypes = /^[А-Яа-я]{2,}/
 export const regCategory = /^[А-Яа-я]{2,}/
 
